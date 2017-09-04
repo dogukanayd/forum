f04428b792f5877be491f6c926e383f885aed507
dogukanayd <daydogdu93@gmail.com>
2017-09-04 08:43:12 +0300

    update

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -1,32 +1,35 @@
 <?php
 
-
 namespace App;
 
+trait Favoritable {
+	protected static function bootFavoritable() {
+		if (!auth()->check()) {
+			return;
+		}
+		static::deleting(function ($model) {
+			$model->favorites->each(function ($model) {
+				$model->delete();
+			});
+		});
+	}
 
-trait Favoritable
-{
+	public function favorites() {
+		return $this->morphMany(Favorite::class, 'favorited');
+	}
 
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
+	public function favorite() {
+		$attributes = ['user_id' => auth()->id()];
+		if (!$this->favorites()->where($attributes)->exists()) {
+			return $this->favorites()->create($attributes);
+		}
+	}
 
-    public function favorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-        if (!$this->favorites()->where($attributes)->exists()) {
-            return $this->favorites()->create($attributes);
-        }
-    }
+	public function isFavorited() {
+		return !!$this->favorites->where('user_id', auth()->id())->count();
+	}
 
-    public function isFavorited()
-    {
-        return !!$this->favorites->where('user_id', auth()->id())->count();
-    }
-
-    public function getFavoritesCountAttribute()
-    {
-        return $this->favorites->count();
-    }
+	public function getFavoritesCountAttribute() {
+		return $this->favorites->count();
+	}
 }
